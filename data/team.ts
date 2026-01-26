0e0ce677bdb515b876d6017e3d18246a35f8d070
Roshith-krishna <roshithkrishnakm.com>
2026-01-26 15:49:56 +0530

    Enhance team page with hover effects, fix theme issues, and optimize animations

---
+++ data/team.ts
@@ -0,0 +1,115 @@
+export interface TeamMember {
+    name: string;
+    role: string;
+    image: string;
+    github?: string;
+    linkedin?: string;
+    twitter?: string;
+}
+
+export const team: TeamMember[] = [
+    {
+        name: "Rishnu Lal N",
+        role: "Core Organizer",
+        image: "/rish.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Hussain Huzefa",
+        role: "Co Organizer",
+        image: "/huss.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Devapriya k",
+        role: "CO Organizer",
+        image: "/DEVA.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Roshith Krishna",
+        role: "Finance Lead",
+        image: "/ROSH.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Anvar Sadath",
+        role: "Design Lead",
+        image: "/anv.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Lakshmi Reji Suresh",
+        role: "Women in Lead",
+        image: "/LAKSHMII.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Rida Waseem",
+        role: "Women in Lead",
+        image: "/RIDA.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Sayanth P",
+        role: "Social Lead",
+        image: "/say.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Ashwandha",
+        role: "CO finance Lead",
+        image: "/ASHWANDHA.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Muhammad Aswlah",
+        role: "Designer",
+        image: "/ASWLAH.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Fathima P",
+        role: "Designer",
+        image: "/FATHIMA.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Sandra Sunil T",
+        role: "Co social Lead",
+        image: "/SANDRA.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+    {
+        name: "Muhammed Sinan A P",
+        role: "content Writer",
+        image: "/SINAN.jpg",
+        github: "https://github.com/",
+        linkedin: "https://linkedin.com/in/",
+        twitter: "https://twitter.com/",
+    },
+];
